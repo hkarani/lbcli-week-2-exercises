64cf10375dc66dd3c8e01612ac1f7664ae7786bd
hkarani <hezronkaranii@gmail.com>
2025-03-27 14:34:43 +0300

    Mine 10 blocks

--- submission/03.sh
+++ submission/03.sh
@@ -1,9 +1,11 @@
 # Created a SegWit address.
 # Add funds to the address.
 # Return only the Address
+# Step 1: Create a new SegWit address (Bech32)
 NEW_ADDRESS=$(bitcoin-cli -regtest getnewaddress "" bech32)
 
 bitcoin-cli -regtest generatetoaddress 101 "$NEW_ADDRESS"
 
-# Output only the new address
+bitcoin-cli -regtest generatetoaddress 10 "$(bitcoin-cli -regtest getnewaddress)"
+
 echo "$NEW_ADDRESS"
