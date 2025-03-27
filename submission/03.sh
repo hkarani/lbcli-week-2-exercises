2a54239fc043371f95862d6d3ba609519fd1ca52
hkarani <hezronkaranii@gmail.com>
2025-03-27 14:27:15 +0300

    Switch to regtest

--- submission/03.sh
+++ submission/03.sh
@@ -1,9 +1,9 @@
 # Created a SegWit address.
 # Add funds to the address.
 # Return only the Address
-NEW_ADDRESS=$(bitcoin-cli getnewaddress "" bech32)
+NEW_ADDRESS=$(bitcoin-cli -regtest getnewaddress "" bech32)
 
 AMOUNT=0.001
-bitcoin-cli sendtoaddress "$NEW_ADDRESS" "$AMOUNT"
+bitcoin-cli -regtest sendtoaddress "$NEW_ADDRESS" "$AMOUNT"
 
 echo "$NEW_ADDRESS"
