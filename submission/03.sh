910249ab5bcb0d985eb3f9fb79b7009060083f9e
hkarani <hezronkaranii@gmail.com>
2025-03-27 14:24:06 +0300

    Create and fund a segwit address

--- submission/03.sh
+++ submission/03.sh
@@ -1,3 +1,9 @@
 # Created a SegWit address.
 # Add funds to the address.
 # Return only the Address
+NEW_ADDRESS=$(bitcoin-cli getnewaddress "" bech32)
+
+AMOUNT=0.001
+bitcoin-cli sendtoaddress "$NEW_ADDRESS" "$AMOUNT"
+
+echo "$NEW_ADDRESS"
